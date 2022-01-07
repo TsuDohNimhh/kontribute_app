63cc5b2cd6fb89a9f80202ba31c30906a323dd94
Jesse Keogh <jessekeogh@jesses-air.home>
2022-01-07 21:09:47 +0000

    text update

--- src/containers/erc-20/Erc-20.jsx
+++ src/containers/erc-20/Erc-20.jsx
@@ -6,7 +6,7 @@ const Erc20 = () => (
     <div className="gpt3__possibility-content">
       <h4>Giving back to the community</h4>
       <h1 className="gradient__text">Earn Kontribute Tokens</h1>
-      <p>Kontribute tokens will be giving as a reward for using the kontribute dapp. Those who vote on the Bonsai story and purchase Bonsai Warrior NFT's will be able to earn Kontribute tokens.</p>
+      <p>Kontribute tokens will be given as a reward for using the Kontribute dapp. Those who vote on the Bonsai story and purchase Bonsai Warrior NFT's will be able to earn Kontribute tokens.</p>
       <h4>More details on the token coming soon...</h4>
     </div>
   </div>
