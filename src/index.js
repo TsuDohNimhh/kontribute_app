e4f7b68543698b373e1194dbff9ed0bcf036747a
Jesse Keogh <jessekeogh@jesses-air.home>
2022-01-10 20:41:54 +0000

    Adding anaylytics

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import ReactGA from 'react-ga';
 
+const TRACKING_ID = "G-GRE5XXJP3H";
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
     <App />,
